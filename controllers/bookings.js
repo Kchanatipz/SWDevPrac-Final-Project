ee6d95c061e8bcb418351a1e9770bc3df52bc635
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-04-04 20:55:29 +0700

    create router file for booking

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -1,19 +1,40 @@
 // desc     Get all bookings
 // route    GET /api/v1/bookings
 // access   Public
+exports.getAllBookings = async (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Show all boookings" });
+};
 
 // desc     Get single booking
 // route    GET /api/v1/bookings/:id
 // access   Public
+exports.getBooking = async (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show boooking ${req.params.id}` });
+};
 
 // desc     Create new booking
 // route    POST /api/v1/bookings
 // access   Private
+exports.createBooking = async (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Create new boooking" });
+};
 
 // desc     Update booking
 // route    PUT /api/v1/bookings/:id
 // access   Private
+exports.updateBooking = async (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update booking ${req.params.id}` });
+};
 
 // desc     Delete booking
 // route    GET /api/v1/bookings/:id
 // access   Private
+exports.deleteBooking = async (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete booking ${req.params.id}` });
+};
--- routes/booking.js
+++ routes/booking.js
@@ -1,36 +1,16 @@
 const express = require("express");
+const {
+  getAllBookings,
+  getBooking,
+  createBooking,
+  updateBooking,
+  deleteBooking,
+} = require("../controllers/bookings");
 
 const router = express.Router();
 const app = express();
 
-// router.get("/", (req, res) => {
-//   res.status(200).json({ success: true, msg: "Hi from server.js" });
-// });
-
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, msg: "Show all boookings" });
-});
-
-router.get("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show boooking ${req.params.id}` });
-});
-
-router.post("/", (req, res) => {
-  res.status(200).json({ success: true, msg: `Create new boooking` });
-});
-
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update booking ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete booking ${req.params.id}` });
-});
+router.route("/").get(getAllBookings).post(createBooking);
+router.route("/:id").get(getBooking).put(updateBooking).delete(deleteBooking);
 
 module.exports = router;
