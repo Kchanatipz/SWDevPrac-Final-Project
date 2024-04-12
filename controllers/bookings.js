46f551e50834ed969d65f0a43dab540a88325ccf
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-04-12 15:31:30 +0700

    create real function for booking

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -1,40 +1,90 @@
+const { json } = require("express");
+const Booking = require("../models/Booking");
+
 // desc     Get all bookings
 // route    GET /api/v1/bookings
 // access   Public
 exports.getAllBookings = async (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Show all boookings" });
+  try {
+    const bookings = await Booking.find();
+
+    res
+      .status(200)
+      .json({ succes: true, count: bookings.length, data: bookings });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ success: false, msg: "Can't find the booking" });
+  }
 };
 
 // desc     Get single booking
 // route    GET /api/v1/bookings/:id
 // access   Public
 exports.getBooking = async (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show boooking ${req.params.id}` });
+  // console.log(req);
+  try {
+    const booking = await Booking.findById(req.params.id);
+
+    if (!booking) {
+      return res.status(400).json({ success: false, msg: "Booking not found" });
+    }
+
+    res.status(200).json({ success: true, data: booking });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      success: false,
+      msg: "HI",
+    });
+  }
 };
 
 // desc     Create new booking
 // route    POST /api/v1/bookings
 // access   Private
 exports.createBooking = async (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Create new boooking" });
+  // console.log(req);
+
+  const booking = await Booking.create(req.body);
+
+  res.status(201).json({ success: true, data: booking });
 };
 
 // desc     Update booking
 // route    PUT /api/v1/bookings/:id
 // access   Private
 exports.updateBooking = async (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update booking ${req.params.id}` });
+  try {
+    const booking = await Booking.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!booking) {
+      return res.status(400).json({ success: false, msg: "Booking not found" });
+    }
+
+    res.status(200).json({ success: true, data: booking });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ success: false });
+  }
 };
 
 // desc     Delete booking
 // route    GET /api/v1/bookings/:id
 // access   Private
 exports.deleteBooking = async (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete booking ${req.params.id}` });
+  try {
+    const booking = await Booking.findByIdAndDelete(req.params.id);
+
+    if (!booking) {
+      return res.status(400).json({ succes: false, msg: "Booking not found" });
+    }
+
+    res.status(200).json({ succes: true, data: {} });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ success: false });
+  }
 };
--- models/Booking.js
+++ models/Booking.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
+
 const BookingSchema = new mongoose.Schema({
   bookingDate: {
     type: Date,
     required: true,
   },
   user: {
-    type: mongoose.Schema.ObjectId,
-    ref: "User",
+    // type: mongoose.Schema.ObjectId,
+    // ref: "User",
+    type: String,
     required: true,
   },
   dentist: {
-    type: mongoose.Schema.ObjectId,
-    ref: "Dentist",
+    // type: mongoose.Schema.ObjectId,
+    // ref: "Dentist",
+    type: String,
     required: true,
   },
   createdAt: {
