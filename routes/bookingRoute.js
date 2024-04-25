4f7b38f9c694f9c1afe0aa5d719bba0f00c63a34
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-04-25 12:43:44 +0700

    fix: format prettier

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -8,10 +8,17 @@ const {
 } = require("../controllers/bookingsController");
 
 const router = express.Router();
-const {protect, authorize}=require('../middleware/auth');
+const { protect, authorize } = require("../middleware/auth");
 const app = express();
 
-router.route("/").get(protect,authorize('admin'),getAllBookings).post(protect,createBooking);
-router.route("/:id").get(protect,getBooking).put(protect,updateBooking).delete(protect,deleteBooking);
+router
+  .route("/")
+  .get(protect, authorize("admin"), getAllBookings)
+  .post(protect, createBooking);
+router
+  .route("/:id")
+  .get(protect, getBooking)
+  .put(protect, updateBooking)
+  .delete(protect, deleteBooking);
 
 module.exports = router;
