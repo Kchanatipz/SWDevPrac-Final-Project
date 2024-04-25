1e98a484dbe8a5dbaa4fee4097f655debecbcea1
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-04-25 12:44:33 +0700

    fix: unique email in stead of telephone number

--- models/UserModel.js
+++ models/UserModel.js
@@ -14,7 +14,6 @@ const UserSchema = new mongoose.Schema({
     required: [true, "Please enter telephone number"],
     minlength: 9, //in case not a mobile phone
     maxlength: 10,
-    unique: true, //make email unique for each user
   },
   email: {
     type: String,
@@ -23,6 +22,7 @@ const UserSchema = new mongoose.Schema({
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please add a valid email",
     ],
+    unique: true,
   },
   role: {
     type: String,
@@ -48,21 +48,21 @@ const UserSchema = new mongoose.Schema({
   },
 });
 //encrypt password using bcrypt
-UserSchema.pre('save',async function(next){
-    const salt=await bcrypt.genSalt(10);
-    this.password=await bcrypt.hash(this.password,salt);
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 //sign jwt and return
-UserSchema.methods.getSignedJwtToken=function(){
-    return jwt.sign({id:this._id},process.env.JWT_SECRET,{
-        expiresIn: process.env.JWT_EXPIRE
-    });
-}
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 //Match user entered password to hashed password in database
-UserSchema.methods.matchPassword=async function(enteredPassword){
-    return await bcrypt.compare(enteredPassword,this.password);
-}
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", UserSchema);
